724dbef2a9f7c174d90f83b8a02e0c7c72a576c3
safepal619 <Safepal619@gmail.com>
2024-06-24 02:20:58 +0100

    add origin on socket

--- utils/corConfig.js
+++ utils/corConfig.js
@@ -3,6 +3,9 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5173',
     'https://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:3000',
+    'https://localhost:3000',
     'localhost:5173',
     'http://127.0.0.1:3000',
     'https://safepal-bg.onrender.com/', //front-end website
