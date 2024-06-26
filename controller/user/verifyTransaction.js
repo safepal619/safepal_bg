61df7a97f8fa3281794c3b16abef911b9f458e4d
safepal619 <Safepal619@gmail.com>
2024-06-26 02:01:14 +0100

    edit transaction verification for withdrawal

--- controller/user/verifyTransaction.js
+++ controller/user/verifyTransaction.js
@@ -21,23 +21,40 @@ module.exports = async (req, res, next) => {
 
     if(data.walletId == "BTC") {
         // user.wallet.Bitcoin.cryptobalance 
-        user.wallet.Bitcoin.fiatbalance +=  data.amount
+        if(data.isDeposit){
+          user.wallet.Bitcoin.fiatbalance +=  data.amount
+        } else {
+          user.wallet.Bitcoin.fiatbalance -=  data.amount
+        }
+
     }
 
 
     if( data.walletId == "ETH") {
         // user.wallet.Ethereum.cryptobalance += data.amount
-        user.wallet.Ethereum.fiatbalance += data.amount
+        if(data.isDeposit){
+          user.wallet.Ethereum.fiatbalance += data.amount
+        } else {
+          user.wallet.Ethereum.fiatbalance -= data.amount
+        }
     }
 
 if(data.walletId == "LTC") {
     // user.wallet.Litecoin.cryptobalance += data.amount
-    user.wallet.Litecoin.fiatbalance += data.amount
+    if(data.isDeposit){
+      user.wallet.Litecoin.fiatbalance += data.amount
+    } else {
+      user.wallet.Litecoin.fiatbalance -= data.amount
+    }
 }
 
 if(data.walletId == "TRC20 USDT"){
     // user.wallet.Tether.cryptobalance +=  data.amount
-    user.wallet.Tether.fiatbalance += data.amount
+    if(data.isDeposit){
+      user.wallet.Tether.fiatbalance += data.amount
+    } else {
+      user.wallet.Tether.fiatbalance -= data.amount
+    }
 }
 
 
